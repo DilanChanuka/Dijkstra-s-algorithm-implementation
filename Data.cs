2d6adf96fe010b768f53c1a323f5923df2474b5f
Dilan <dilanchanukalasantha@gmail.com>
2020-10-20 21:43:54 +0530

    Add project files.

---
+++ Data.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra_s_algorithm
+{
+    public static class Data
+    {
+        #region PreDefined DataSet
+
+        public static int Vertices = 9;
+        
+        public static int[,] Endpoints = new int[,]
+        {
+            {0,1 },{1,2 },{2,3 },{3,4 },{ 4,5},{5,6 },{6,7 },{0,7 },{1,7 },{ 7,8},{ 6,8},{2,8 },{2,5 },{ 3,5}
+        };    
+
+        public static int[,] Graph = new int[,] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+                                                      { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                                                      { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                                                      { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                                                      { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                                                      { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                                                      { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                                                      { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                                                      { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+        
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra_s_algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 3;
+            int end = 7;
+            int[] routeNumbers = GetRouteNumbers(start, end);
+            Console.WriteLine("Route numbers to move from " + start + " to " + end + "\n");
+
+            foreach (int rote in routeNumbers)
+            {
+                Console.Write(rote + "\t");
+            }
+            Console.ReadLine();
+        }
+        public static int[] GetRouteNumbers(int start, int end)
+        {
+            FindShortestPath find = new FindShortestPath();
+            List<int[]> allPaths = find.GetShortestPathList(Data.Graph, Data.Vertices, start);
+            int[] path = allPaths[end];
+            List<int> routeNumbers = new List<int>();
+
+            for (int j = 0; j < path.Length - 1; j++)
+            {
+                for (int i = 0; i < Data.Endpoints.GetLength(0); i++)
+                {
+                    if ((Data.Endpoints[i, 0] == path[j] && Data.Endpoints[i, 1] == path[j + 1]) ||
+                        (Data.Endpoints[i, 1] == path[j] && Data.Endpoints[i, 0] == path[j + 1]))
+                    {
+                        routeNumbers.Add(i);
+                        break;
+                    }
+                }
+            }
+            return routeNumbers.ToArray();
+        }
+    }
+}
